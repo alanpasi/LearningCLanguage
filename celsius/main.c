4a564400d1407c5028a5b324c7c6ca3aad65317f
alanpasi <alanpasi@gmail.com>
2018-08-07 11:59:56 -0300

    Incluido separadores verticais

--- celsius/main.c
+++ celsius/main.c
@@ -1,23 +1,21 @@
 #include <stdio.h>
-//#include<stdlib.h>
+
+#define INICIO      0       // Limite inferior da tabela
+#define FIM          100   // Limite superior da tabela
+#define INCR        5        // Incremento
 
 int main(int argc, char **argv)
 {
-    int inicio, fim, incr;
     float fahr, celsius;
     
-    inicio = 0;
-    fim = 100;
-    incr = 5;
+    fahr = INICIO;
     
-    fahr = inicio;
+    printf("Converte Fahrenheit para Celsius de %3d  até %3d  Fahrenheit\n", INICIO, FIM);
     
-    printf("Converte Fahrenheit para Celsius de %3d  até %3d  Fahrenheit\n", inicio, fim);
-    
-    while(fahr <= fim){
+    while(fahr <= FIM){
         celsius = (5.0/9.0)*(fahr - 32.0);
-        printf("%4.0f %6.1f\n", fahr, celsius);
-        fahr = fahr + incr;
+        printf("|%4.0f\t|%6.1f|\n", fahr, celsius);
+        fahr = fahr + INCR;
     }
 	return 0;
 }
