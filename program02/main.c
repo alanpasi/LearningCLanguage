c50ea14319a575c2a617438c31d7012292a8f26b
alanpasi <alanpasi@gmail.com>
2018-08-09 22:13:11 -0300

    Testando...

--- program02/main.c
+++ program02/main.c
@@ -5,9 +5,8 @@
 float bhaskara(float a, float b, float c);
 
 int main() {
-	printf("Hello World! \n");
     
-    bhaskara(1,2,3);
+    bhaskara(0,2,3);
     
 	return 0;
 }
@@ -18,20 +17,16 @@ float bhaskara(float a, float b, float c) {
  
 	if (a == 0)
 		printf("Não é uma equação do segundo grau, pois a = 0\n");
-
 	else {
 		delta  = (b*b)-4*a*c;
 		if (delta < 0) {
 			delta = delta * (-1);
 			x1_real = -b/(2*a);
 			x1_imag = sqrt(delta)/(2*a);
-
 			x2_real = -b/(2*a);
 			x2_imag = -sqrt(delta)/(2*a);
-
 			printf("x1 = %.4f +%.4fi \n", x1_real, x1_imag);
 			printf("x2 = %.4f %.4fi \n", x2_real, x2_imag);
-
 		}
 			else {
 				x1_real = (-b + sqrt(delta))/2*a;
@@ -39,7 +34,5 @@ float bhaskara(float a, float b, float c) {
 				printf("x1 = %.4f \n", x1_real);
 				printf("x2 = %.4f \n", x2_real);
 			}
-
 	} // end else
-		
 } // end bhaskara
