6b1fe0c6e546bb300eedbe0ce74da9b23f8cedbe
alanpasi <alanpasi@gmail.com>
2018-08-10 15:38:33 -0300

    Segunda versão da função potência

--- power/main.c
+++ power/main.c
@@ -9,11 +9,22 @@ int main(int argc, char **argv)
     int i;
     
     for(i = 0; i < 10; ++i) {
-        printf("%2d %3d %6d\n", i, power(2, i), power(-3, i));
+        printf("%2d %3d %6d\n", i, pot(2, i), pot(-3, i));
+//        printf("%2d %3d %6d\n", i, power(2, i), power(-3, i));
     }
 	return 0;
 }
 
+pot(x, n)
+int x, n;
+{
+    int p;
+    for(p = 1; n > 0; --n)
+        p = p * x;
+    return(p);
+}
+
+/*
 int power(int base, int n) {
     int i, p;
     
@@ -22,4 +33,4 @@ int power(int base, int n) {
     for(i = 1; i <= n; ++i)
         p = p * base;
     return p;
-}
+}*/
